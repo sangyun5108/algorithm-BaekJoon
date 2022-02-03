ed3c7ed5ec08ac36005af3686b0166aac3493010
Sangyun <tkfkd4447@naver.com>
2022-02-03 15:54:17 +0900

    토마토문제

---
+++ 2주차_DFS와BFS/sangyun5108/7_토마토/7569.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+m,n,h = map(int,input().split()) # mn크기, h상자수
+graph = []
+queue = deque([])
+ 
+for i in range(h):
+    tmp = []
+    for j in range(n):
+        tmp.append(list(map(int,sys.stdin.readline().split())))
+        for k in range(m):
+            if tmp[j][k]==1:
+                queue.append([i,j,k])
+    graph.append(tmp)
+    
+dx = [-1,1,0,0,0,0]
+dy = [0,0,1,-1,0,0]
+dz = [0,0,0,0,1,-1]
+while(queue):
+    x,y,z = queue.popleft()
+    
+    for i in range(6):
+        nx = x+dx[i]
+        ny = y+dy[i]
+        nz = z+dz[i]
+        if 0<=nx<h and 0<=ny<n and 0<=nz<m and graph[nx][ny][nz]==0:
+            queue.append([nx,ny,nz])
+            graph[nx][ny][nz] = graph[x][y][z]+1
+            
+result = 0
+for i in graph:
+    for j in i:
+        for k in j:
+            if k==0:
+                print(-1)
+                exit(0)
+        result = max(result,max(j))
+print(result-1)
